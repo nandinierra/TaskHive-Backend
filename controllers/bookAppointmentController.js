72a030c56d5e24bf8b7bdba4f94f1a2b10eaca16
nandinierra <nandinierra34@gmail.com>
2025-09-20 09:13:59 +0530

    .

--- controllers/bookAppointmentController.js
+++ controllers/bookAppointmentController.js
@@ -7,8 +7,9 @@ export async function bookAppointment(req,res) {
     try{
     const userId = req.user.id 
      const {technicianId} = req.body 
+     console.log(`from backend ${technicianId}`)
      const appointment = await appointmentModel.create({technicianId,userId})
-     return res.status(201).json({message:"Appointment booked sucessfully..."},appointment)
+     return res.status(201).json({message:"Appointment booked sucessfully..."}, appointment)
     }
 
     catch(e) {
@@ -31,14 +32,16 @@ export async function getAppointments(req,res){
 }
 
 
+
 export async function deleteAppointments(req,res){
   try{
      const appointmentId = req.params.appointmentId;
+       const userId=req.user.id;
      console.log(appointmentId);
-     const appointment=await appointmentModel.findOneAndDelete({technicianId:appointmentId});
-     res.status(201).json({
-      deleteAppointment:appointment
-     })
+    const appointment= await appointmentModel.findByIdAndDelete(appointmentId);
+     const updatedAppointments= await appointmentModel.find({userId}).populate("technicianId", "name profession experience image")
+    
+     res.status(201).json(updatedAppointments)
   }
   catch(error){
     res.status(400).json({
